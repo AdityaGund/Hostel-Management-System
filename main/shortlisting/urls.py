e679a1d56cda50476115c68370d775d8d4eb0e15
AdityaGund <aditya.u.gund@gmail.com>
2024-04-11 11:36:47 +0530

    Room allocation logic and pdf generation added !!

--- main/shortlisting/urls.py
+++ main/shortlisting/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('roommate_requests/', roommate_requests, name='roommate_requests'),
     path('studentHome/', studentHome, name="studentHome"),
     path('room_preferences/', room_preferences, name="room_preferences"),
+    # path('room_allocation/',roomAllocation,name='room_allocation'),
+    path('generate_pdf2/',generate_pdf2,name='generate_pdf2'),
+    path('select_students/',select_students,name="select_students"),
+    path('allot_rooms/',allot_rooms,name="allot_rooms"),
 ]
